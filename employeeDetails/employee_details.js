25dedca678fcfeb766960d6b51c579044f3e7d18
Bhavish511 <Bhavishsindhi3@gmail.com>
2024-12-27 01:49:52 +0500

    Added new thing in EmployeeDetails search by specialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,19 +1,24 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000,specialization:'JavaScript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000,specialization:'JavaScript' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000,specialization:'Java' },
     //... More employee records can be added here
 ];
 
 function displayEmployees() {
      // Function to display all employees
-    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
 
 function calculateTotalSalaries() {
     const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
     alert(`Total Salaries: $${totalSalaries}`);
+}
+function findspecialization() {
+    const foundemp = employees.filter(employee => employee.specialization === 'JavaScript');
+    const specialEmployeesDisplay = foundemp.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = specialEmployeesDisplay;
 }
 
 function displayHREmployees() {
